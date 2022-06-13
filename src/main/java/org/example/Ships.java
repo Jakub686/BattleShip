c5ac30be8bd0d27e6ce2fdd94491f23f3f972953
Jakub <jakub.bchyla@gmail.com>
2022-06-13 15:06:52 +0200

    added ships

---
+++ src/main/java/org/example/Ships.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Ships {
+    static int[] ship1Five = new int[11]; // This ship is made of five parts OOOOOO 1 player
+    static int[] ship1Four = new int[9]; // This ship is made of four parts  OOOO 1 player
+    static int[] ship1Three1 = new int[7]; // This ship is made of three parts OOO 1 player
+    static int[] ship1Three2 = new int[7]; // This ship is made of three parts OOO 1 player
+    static int[] ship1Two = new int[5]; // This ship is made of two parts OO 1 player
+    static int[] ship2Five = new int[11]; // This ship is made of five parts OOOOOO 2 player
+    static int[] ship2Four = new int[9]; // This ship is made of four parts  OOOO 2 player
+    static int[] ship2Three1 = new int[7]; // This ship is made of three parts OOO 2 player
+    static int[] ship2Three2 = new int[7]; // This ship is made of three parts OOO 2 player
+    static int[] ship2Two = new int[5]; // This ship is made of two parts OO 2 player
+}

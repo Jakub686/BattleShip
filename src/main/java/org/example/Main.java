3fff0e7698b250b4fea2e5eb07579b4474067795
Jakub <jakub.bchyla@gmail.com>
2022-06-14 19:23:47 +0200

    refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,14 @@
 package org.example;
 
-import org.example.model.Grid;
-import org.example.view.Show;
+import org.example.view.Grid;
 
 public class Main
 {
     public static void main( String[] args )
     {
-        String[][] grid1 = new Grid().getGrid(16, 16);
-        String[][] grid2 = new Grid().getGrid(16, 16);
-        Show.showGrid(grid1);
-        Show.showGridFog(grid2);
+        String[][] grid1 = new org.example.model.Grid().getGrid(16, 16);
+        String[][] grid2 = new org.example.model.Grid().getGrid(16, 16);
+        Grid.showGrid(grid1);
+        Grid.showGridFog(grid2);
     }
 }

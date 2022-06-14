8a867599b3d2d113bad177bf003c88117ebfd05f
Jakub <jakub.bchyla@gmail.com>
2022-06-14 17:00:31 +0200

    last working

---
+++ Battleship/task/src/battleship/Grid.java
@@ -0,0 +1,22 @@
+package battleship;
+
+public class Grid {
+    public String[][] getGrid(int row, int column) {
+        String[][] grid = new String[row][column];
+        grid[0][0] = " ";
+        for (int i = 1; i < column; i++) {
+            grid[0][i] = String.valueOf(i);
+        }
+        char c = 'A';
+        for (int i = 1; i < row; i++) {
+            grid[i][0] = String.valueOf(c);
+            c++;
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                grid[i][j] = "~";
+            }
+        }
+        return grid;
+    }
+}

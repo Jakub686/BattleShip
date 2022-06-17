639752f50a6eb2fb90d5af695a600e2db044fc88
Jakub <jakub.bchyla@gmail.com>
2022-06-17 08:35:00 +0200

    Refactored ShipSetter, created CoordinatesSetter.class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.shipSetter.ShipSetter;
 import org.example.view.Grid;
 import org.example.view.GridFog;
+
+import java.util.Scanner;
 
 public class Main
 {
@@ -13,5 +16,17 @@ public class Main
         //TODO musze stworzyc nowy obiekty, bo Grid i GridFog implementuja IGrid, a w interfacie metoda statyczna musi miec cialo, a chyba zalezalo nam na tym aby nie miala ciala
         new Grid().show(grid1);
         new GridFog().show(grid2);
+
+
+        System.out.println("Player 1, place your ships on the game field");
+        new ShipSetter().shipSetup(grid1, Ships.ship1Five, Ships.ship1Four, Ships.ship1Three1, Ships.ship1Three2, Ships.ship1Two);
+
+        System.out.println("Press Enter and pass the move to another player");
+        Scanner sc = new Scanner(System.in);
+        String anotherPlayer = sc.nextLine();
+        //Setting ship for player 2
+        System.out.println("Player 2, place your ships to the game field");
+        if (anotherPlayer.equals(""))
+            new ShipSetter().shipSetup(grid2, Ships.ship2Five, Ships.ship2Four, Ships.ship2Three1, Ships.ship2Three2, Ships.ship2Two);
     }
 }

e9d35094ddafbcfb21d28df4bba96ad33364f8cc
Jakub <jakub.bchyla@gmail.com>
2022-07-07 18:51:30 +0200

    Made shipsetter and shot more generic

--- src/main/java/com/github/jakub686/Main.java
+++ src/main/java/com/github/jakub686/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
         //Player 1 ships
         ArrayList<Ship> shipListPlayer1 = new ArrayList<>();
 
